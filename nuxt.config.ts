526f8991c28a894c1e5d5c69ffd9a3ab95eefce5
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-09-20 23:35:16 -0300

    feat: installed PrimeVue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  css: [
+    'primevue/resources/themes/saga-blue/theme.css',
+    'primevue/resources/primevue.css',
+    'primeicons/primeicons.css',
+  ],
 })
