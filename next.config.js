8e82b0a26e90e969b1d3d3b4630c3a0d266dc025
Nicolas Lima <nicolas.lima@redealumni.com>
2022-11-03 21:51:37 -0300

    feat: removed components usage from @izanagi-ui and started to create from scratch

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "variables/index.module.scss";`,
+  },
 }
 
 module.exports = nextConfig
